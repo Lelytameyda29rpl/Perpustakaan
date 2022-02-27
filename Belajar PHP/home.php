b651f3a62cc0a7e770ce25f4028738dfb9cf6fe6
Lelytameyda29rpl <72966988+Lelytameyda29rpl@users.noreply.github.com>
2022-02-27 19:43:00 +0700

    Add files via upload

---
+++ Belajar PHP/home.php
@@ -0,0 +1,16 @@
+<?php
+include "header.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-color: lightblue;
+}
+</style>
+</head>
+</br>
+<body>
+<h2 align="center">Selamat datang <?=$_SESSION['nama_siswa']?> di website Perpus Online.</h2>
+</body>
